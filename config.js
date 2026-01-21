1d55c396fdfb333b61ce1608faab9d451ff113a0
thomaslancherospinnacle-rgb <thomaslancheros.pinnacle@gmail.com>
2026-01-20 23:58:23 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
-const CFworker = "https://closing-coalition-api.thomaslancheros06.workers.dev";
-const refDB = "https://script.google.com/macros/s/AKfycbyFC2gs5saqN_2wVlktcC0PrFHceoPKhep_SvYN7380FOeo4f-D962M59dkpk0JKZASdw/exec";
-const appDB = "https://script.google.com/macros/s/AKfycbxcoHmU_iqzvcmawu6kBuN0_Tmb4gWGk8v7MaHxzKzn1FhfVA_MDiqBU1otwgxCL9NM/exec";
+const CFWORKER = "https://closing-coalition-api.thomaslancheros06.workers.dev";
+const REFDB = "https://script.google.com/macros/s/AKfycbyFC2gs5saqN_2wVlktcC0PrFHceoPKhep_SvYN7380FOeo4f-D962M59dkpk0JKZASdw/exec";
+const APPDB = "https://script.google.com/macros/s/AKfycbxcoHmU_iqzvcmawu6kBuN0_Tmb4gWGk8v7MaHxzKzn1FhfVA_MDiqBU1otwgxCL9NM/exec";
